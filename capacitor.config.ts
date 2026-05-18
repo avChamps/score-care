99e848d952cbc9b40c8aad43285f557d0d2aa2f4
AC CHamps <avchamps1@gmail.com>
2026-05-18 08:02:23 +0530

    changes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "in.scorecare.app",
+  appName: "ScoreCare",
+  webDir: "out",
+};
+
+export default config;
