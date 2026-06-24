c86f8b988cd653be1a71336896771de502852a04
Disendra Yadav <disendra123@gmail.com>
2026-06-24 15:11:56 +0530

    chahges

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -6,6 +8,9 @@ const nextConfig = {
   },
   devIndicators: false,
   allowedDevOrigins: ['192.168.1.7'],
+  turbopack: {
+    root: path.join(__dirname),
+  },
 }
 
 module.exports = nextConfig
